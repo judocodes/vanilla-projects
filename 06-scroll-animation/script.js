7047402e374758a8fa3ce69996ac79875bd64481
Julian <juliandomke@outlook.de>
2021-02-10 09:49:20 +0100

    Scroll into view project with Intersection Observer API

---
+++ 06-scroll-animation/script.js
@@ -0,0 +1,43 @@
+(function () {
+    function ScrollIntoView(element) {
+        this.element = element;
+        this.threshold = 0.5;
+        this.observeElement();
+    }
+
+    ScrollIntoView.prototype = {
+        observeElement() {
+            var observer = this.getObserver();
+            observer.observe(this.element);
+        },
+        getObserver() {
+            var options = {
+                rootMargin: '0% 2000% 0% 2000%',
+                threshold: this.threshold,
+            };
+            return new IntersectionObserver(
+                this.checkIfInView.bind(this),
+                options
+            );
+        },
+        checkIfInView(entries, observer) {
+            entries.forEach(checkIfIntersecting.bind(this));
+
+            console.log(observer);
+
+            function checkIfIntersecting(entry) {
+                console.log(entry);
+                if (entry.isIntersecting) {
+                    this.element.classList.add('show');
+                } else {
+                    this.element.classList.remove('show');
+                }
+            }
+        },
+    };
+
+    var boxes = document.querySelectorAll('.box');
+    boxes.forEach(function applyScrollIntoView(box) {
+        new ScrollIntoView(box);
+    });
+})();
